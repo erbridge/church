c5b5c7ac4695612b07ca95737ce6f444fac3264b
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-04-22 13:54:50 +0100

    Create an automatic letterboxing window

--- src/App.js
+++ src/App.js
@@ -3,26 +3,20 @@ import { Provider } from 'react-redux';
 
 import { createStore } from './store';
 
+import Window from './components/Window';
+
 const store = createStore();
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div style={{ textAlign: 'center' }}>
-          <div
-            style={{
-              backgroundColor: '#222',
-              height: 150,
-              padding: 20,
-              color: 'white',
-            }}
-          >
-            <h2>Welcome to React</h2>
-          </div>
-          <p style={{ fontSize: 'large' }}>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+        <div
+          style={{ width: '100%', height: '100%', backgroundColor: 'black' }}
+        >
+          <Window style={{ backgroundColor: 'white' }}>
+            <div>This is the story!</div>
+          </Window>
         </div>
       </Provider>
     );
---
+++ src/components/Window.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Window extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    ratio: PropTypes.number,
+    style: PropTypes.object,
+  };
+
+  static defaultProps = {
+    ratio: 16 / 9,
+  };
+
+  state = {
+    height: 0,
+    left: 0,
+    top: 0,
+    width: 0,
+  };
+
+  updateDimensions(props) {
+    const { ratio } = props;
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const screenRatio = screenWidth / screenHeight;
+
+    let width = screenWidth;
+    let height = screenHeight;
+
+    if (screenRatio > ratio) {
+      width = screenHeight * ratio;
+    } else {
+      height = screenWidth / ratio;
+    }
+
+    this.setState({
+      left: (screenWidth - width) / 2,
+      top: (screenHeight - height) / 2,
+      width,
+      height,
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateDimensions(nextProps);
+  }
+
+  componentWillMount() {
+    this.updateDimensions(this.props);
+  }
+
+  componentDidMount() {
+    this.onResize = () => this.updateDimensions(this.props);
+
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+
+    delete this.onResize;
+  }
+
+  render() {
+    const { children, style } = this.props;
+    const { height, left, top, width } = this.state;
+
+    return (
+      <div style={{ ...style, position: 'absolute', left, top, width, height }}>
+        {children}
+      </div>
+    );
+  }
+}
